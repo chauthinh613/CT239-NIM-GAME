1556d5ac18ee81a2d05ec153553bc5b0f7f69bcb
aditya <chauthinh613@gmail.com>
2026-01-12 23:55:10 +0700

    Creat first test and nim game

---
+++ TestGame/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TestGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(53, 44);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(458, 135);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(64, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(154, 59);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = Properties.Resources.logo_hoi_sinh_vien_inkythuatso_01;
+            button2.BackgroundImageLayout = ImageLayout.Zoom;
+            button2.Location = new Point(536, 193);
+            button2.Name = "button2";
+            button2.Size = new Size(220, 225);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ TestGame/Form1.cs
@@ -0,0 +1,52 @@
+﻿namespace TestGame
+{
+    public partial class Form1 : Form
+    {
+        int itemWidth = 50;
+        int itemHeigh = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear(); //xoá hết tất cả trong panel
+
+
+            for(int i = 1; i <= 5; i++)
+            {
+                Button btn = new Button();
+                btn.Text = i.ToString();
+                btn.Width = itemWidth;
+                btn.Height = itemHeigh;
+                btn.Top = 10;
+                btn.Left = 10 + (itemWidth + 5) * i;
+                //btn.BackColor = Color.Transparent;
+                btn.FlatStyle = FlatStyle.Flat;        
+                //btn.FlatAppearance.BorderSize = 0;
+                btn.BackgroundImage = Image.FromFile(@"D:/Download/bolt.png");
+                btn.BackgroundImageLayout = ImageLayout.Zoom;
+                btn.FlatAppearance.BorderSize = 0;
+
+                btn.TabStop = false;
+
+                btn.Click += Btn_Click;
+
+                panel1.Controls.Add(btn);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button clickedBtn = (Button)sender;
+            clickedBtn.Visible = false;
+        }
+    }
+}
